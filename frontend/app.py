563801ce8c4710122373c3f88bfedc032b8f5f57
Pedro Coelho <pedrocoelho887@gmail.com>
2026-06-27 20:36:43 -0300

    Adição de Tela de informações de endereço

--- frontend/app.py
+++ frontend/app.py
@@ -1,17 +1,16 @@
 import tkinter as tk
 from tkinter import ttk
 from backend.storage import Storage
-from frontend.frames import MainFrame,FillInfoFrame
+from frontend.frames import MainFrame,FillInfoFrame,InfoFrame
 """Classe principal do App"""
 class App:
     def __init__(self):
         self.janela = tk.Tk()
         self.janela.title("Catálogo de profissionais")
         self.mainframe = MainFrame(self.janela,self.open_newprofile_window,self.delete_profile,self.open_updateprofile_window)
-        # self.newprofileframe = NewProfileFrame(self.janela,self.open_main_window,self.add_profile)
-        # self.updateprofileframe = UpdateProfileFrame(self.janela,self.open_main_window,self.update_profile)
         self.newprofileframe = FillInfoFrame(self.janela,self.open_main_window,self.add_profile,"Criar Novo Perfil","Criação de Perfil")
         self.updateprofileframe = FillInfoFrame(self.janela,self.open_main_window,self.update_profile,"Atualizar Perfil","Atualização Perfil")
+        self.infoframe = InfoFrame(self.janela,self.open_main_window)
         self.storage = Storage("backend/perfis.txt","backend/enderecos.txt")
 
     def open_main_window(self):
@@ -61,14 +60,28 @@ class App:
         else:
             self.updateprofileframe.warning_label.config(text="Perfil Atualizado!")
 
+    def display_infoframe(self,event):
+        selected = self.mainframe.tabela.selection()
+
+        if not selected:
+            return
+        item = selected[0]
+        id = self.mainframe.tabela.item(item, "values")[0]
+        profile = self.storage.database[id]
+
+        self.infoframe.update_window(profile)
+        self.infoframe.frame.tkraise()
+
     def run(self):
         self.mainframe.frame.grid(row=0,column=0,sticky="nsew")
         self.newprofileframe.frame.grid(row=0,column=0,sticky="nsew")
         self.updateprofileframe.frame.grid(row=0,column=0,sticky="nsew")
+        self.infoframe.frame.grid(row=0,column=0,sticky="nsew")
 
-        self.mainframe.build_window()
+        self.mainframe.build_window(self.display_infoframe)
         self.newprofileframe.build_window()
         self.updateprofileframe.build_window()
+        self.infoframe.build_window()
 
         self.open_main_window()
         self.janela.mainloop()
--- frontend/frames.py
+++ frontend/frames.py
@@ -17,8 +17,8 @@ class MainFrame:
         self.create_profilebutton = tk.Button(self.frame,text="Novo Perfil",command=open_newprofile_window)
         self.deletebutton = tk.Button(self.frame,text="Deletar Perfil",command=delete_profile)
         self.update_profilebutton = tk.Button(self.frame,text="Atualizar Perfil",command=update_profile)
-        self.label = tk.Label(self.frame,text="Bem vindo ao catálogo de profissionais!")
-    
+        self.title = tk.Label(self.frame,text="Bem vindo ao catálogo de profissionais!")
+        self.details = tk.Label(self.frame,text="Para Saber mais, clique 2 vezes no perfil desejado.")
     def update_table(self,database):
         # Remove todas as linhas atuais
         for row in self.tabela.get_children():
@@ -28,140 +28,20 @@ class MainFrame:
         for id, perfil in database.items():
             self.tabela.insert("", tk.END, values=(id, perfil.nome, perfil.cpf_cnpj,perfil.nascimento,perfil.contato))
 
-    def build_window(self):
-        self.label.pack(pady=10)
+    def build_window(self,display_infoframe):
+        self.title.pack(pady=10)
         self.tabela.pack(pady=10)
+        self.tabela.bind("<Double-1>",display_infoframe)
         self.create_profilebutton.pack(pady=10,side="left")
         self.deletebutton.pack(pady=10,side="left")
         self.update_profilebutton.pack(pady=10,side="left")
-
-"""Classe do Frame utilizado para criar Novo usuário"""
-class NewProfileFrame:
-    def __init__(self,janela,update_main_window,add_user):
-        self.frame = tk.Frame(janela)
-        self.label = tk.Label(self.frame,text="Cadastro de novo profissional")
-        self.newprofile_label = tk.Label(self.frame,text="")
-        self.personal_info = {"Nome": tk.StringVar(),"CPF/CNPJ":tk.StringVar(),"Data de Nascimento":tk.StringVar(),"Contato (Com DDD)": tk.StringVar()}
-        self.adress_info = {"CEP":tk.StringVar(),"Rua":tk.StringVar(),"Numero":tk.StringVar(),
-                            "Complemento":tk.StringVar(),"Bairro":tk.StringVar(),"Cidade":tk.StringVar(),"UF":tk.StringVar()}
-        self.returnbutton = tk.Button(self.frame,text="Retornar",command=update_main_window)
-        self.confirmbutton = tk.Button(self.frame,text="Criar Novo Perfil",command= lambda:add_user(self.personal_info,self.adress_info))
-        self.searchCEPbutton = tk.Button(self.frame,text="Buscar CEP",command=self.get_address)
-
-    def get_address(self):
-        response = requests.get(f"https://viacep.com.br/ws/{self.adress_info["CEP"].get()}/json/")
-
-        if response.status_code != 200:
-            return
-        
-        dados = response.json()
-        if "erro" in dados:
-            return
-        self.adress_info["Rua"].set(dados["logradouro"])
-        self.adress_info["Bairro"].set(dados["bairro"])
-        self.adress_info["Cidade"].set(dados["localidade"])
-        self.adress_info["UF"].set(dados["uf"])
-        
-
-    def update_window(self):
-        # Limpa todos os campos de input
-        for var in self.personal_info.values():
-            var.set("")
-        self.personal_info["Data de Nascimento"].set("DD/MM/YYYY")
-
-        for var in self.adress_info.values():
-            var.set("")
-        # Limpa a mensagem de feedback
-        self.newprofile_label.config(text="")
-    
-    def build_window(self):
-        self.label.grid(row=0,columnspan=2,pady=10)
-
-        for i, (desc,var) in enumerate(self.personal_info.items()):
-            tk.Label(self.frame,text=desc+":").grid(row=i+1,column=0,sticky="e",padx=5,pady=3)
-            tk.Entry(self.frame,textvariable=var,width=30).grid(row=i+1,column=1,padx=5,pady=3)
-        
-        for i, (desc,var) in enumerate(self.adress_info.items()):
-            tk.Label(self.frame,text=desc+":").grid(row=i+1,column=2,sticky="e",padx=5,pady=3)
-            tk.Entry(self.frame,textvariable=var,width=30).grid(row=i+1,column=3,padx=5,pady=3)
-
-        self.returnbutton.grid(row=6,column=0,pady=10)
-        self.confirmbutton.grid(row=6,column=1,pady=10)
-        self.newprofile_label.grid(row=7,column=1,pady=10)
-        self.searchCEPbutton.grid(row = 1, column=4)
-
-"""Classe do Frame de atualização de Perfil"""
-class UpdateProfileFrame:
-    def __init__(self,janela,update_main_window,update_profile):
-        self.frame = tk.Frame(janela)
-        self.label = tk.Label(self.frame,text="Atualização de Cadastro")
-        self.updateprofile_label = tk.Label(self.frame,text="")
-        self.personal_info = {"Nome": tk.StringVar(),"CPF/CNPJ":tk.StringVar(),"Data de Nascimento":tk.StringVar(),"Contato (Com DDD)": tk.StringVar()}
-        self.adress_info = {"CEP":tk.StringVar(),"Rua":tk.StringVar(),"Numero":tk.StringVar(),
-                            "Complemento":tk.StringVar(),"Bairro":tk.StringVar(),"Cidade":tk.StringVar(),"UF":tk.StringVar()}
-        self.update_id = None
-        self.returnbutton = tk.Button(self.frame,text="Retornar",command=update_main_window)
-        self.confirmbutton = tk.Button(self.frame,text="Atualizar Perfil",command= lambda:update_profile(self.update_id,self.personal_info,self.adress_info))
-        self.searchCEPbutton = tk.Button(self.frame,text="Buscar CEP",command=self.get_address)
-    
-    def get_address(self):
-        response = requests.get(f"https://viacep.com.br/ws/{self.adress_info["CEP"].get()}/json/")
-
-        if response.status_code != 200:
-            return
-        
-        dados = response.json()
-        if "erro" in dados:
-            return
-        self.adress_info["Rua"].set(dados["logradouro"])
-        self.adress_info["Bairro"].set(dados["bairro"])
-        self.adress_info["Cidade"].set(dados["localidade"])
-        self.adress_info["UF"].set(dados["uf"])
-
-    def update_window(self,profile):
-        # Atualiza todos os campos de input
-        self.update_id = profile.id
-        self.personal_info["Nome"].set(profile.nome)
-        self.personal_info["CPF/CNPJ"].set(profile.cpf_cnpj)
-        self.personal_info["Data de Nascimento"].set(profile.nascimento)
-        self.personal_info["Contato (Com DDD)"].set(profile.contato)
-
-        self.adress_info["CEP"].set(profile.CEP)
-        self.adress_info["Rua"].set(profile.rua)
-        self.adress_info["Numero"].set(profile.numero)
-        self.adress_info["Complemento"].set(profile.complemento)
-        self.adress_info["Bairro"].set(profile.bairro)
-        self.adress_info["Cidade"].set(profile.cidade)
-        self.adress_info["UF"].set(profile.UF)
-
-        # Limpa a mensagem de feedback
-        self.updateprofile_label.config(text="")
-
-    def build_window(self):
-        self.label.grid(row=0,columnspan=2,pady=10)
-
-        for i, (desc,var) in enumerate(self.personal_info.items()):
-            tk.Label(self.frame,text=desc+":").grid(row=i+1,column=0,sticky="e",padx=5,pady=3)
-            tk.Entry(self.frame,textvariable=var,width=30).grid(row=i+1,column=1,padx=5,pady=3)
-        
-        for i, (desc,var) in enumerate(self.adress_info.items()):
-            tk.Label(self.frame,text=desc+":").grid(row=i+1,column=2,sticky="e",padx=5,pady=3)
-            tk.Entry(self.frame,textvariable=var,width=30).grid(row=i+1,column=3,padx=5,pady=3)
-
-        self.returnbutton.grid(row=6,column=0,pady=10)
-        self.confirmbutton.grid(row=6,column=1,pady=10)
-        self.updateprofile_label.grid(row=7,column=1,pady=10)
-        self.searchCEPbutton.grid(row = 1, column=4)
-
-
-
-
+        self.details.pack(pady=10)
 
 """Classe do Frame utilizado registrar informação"""
 class FillInfoFrame:
     def __init__(self,janela,update_main_window,confirm_func,text_confirmbutton,text_mainlabel):
         self.frame = tk.Frame(janela)
-        self.label = tk.Label(self.frame,text=text_mainlabel)
+        self.title = tk.Label(self.frame,text=text_mainlabel)
         self.warning_label = tk.Label(self.frame,text="")
         self.id = None
         self.personal_info = {"Nome": tk.StringVar(),"CPF/CNPJ":tk.StringVar(),"Data de Nascimento":tk.StringVar(),"Contato (Com DDD)": tk.StringVar()}
@@ -214,7 +94,7 @@ class FillInfoFrame:
         self.warning_label.config(text="")
     
     def build_window(self):
-        self.label.grid(row=0,columnspan=2,pady=10)
+        self.title.grid(row=0,columnspan=2,pady=10)
 
         for i, (desc,var) in enumerate(self.personal_info.items()):
             tk.Label(self.frame,text=desc+":").grid(row=i+1,column=0,sticky="e",padx=5,pady=3)
@@ -228,3 +108,38 @@ class FillInfoFrame:
         self.confirmbutton.grid(row=6,column=1,pady=10)
         self.warning_label.grid(row=7,column=1,pady=10)
         self.searchCEPbutton.grid(row = 1, column=4)
+
+class InfoFrame:
+    def __init__(self,janela,update_main_window):
+        self.frame = tk.Frame(janela)
+        self.title = tk.Label(self.frame,text="Detalhes do Profissional")
+        self.personal_infos = [tk.Label(self.frame,text="") for _ in range(5)]
+        self.adress_infos = [tk.Label(self.frame,text="")for _ in range(7)]
+        self.returnbutton = tk.Button(self.frame,text="Retornar",command=update_main_window)
+
+    def update_window(self,profile):
+
+        self.personal_infos[0].config(text=f"Id:{profile.id}")
+        self.personal_infos[1].config(text=f"Nome:{profile.nome}")
+        self.personal_infos[2].config(text=f"CPF/CNPJ:{profile.cpf_cnpj}")
+        self.personal_infos[3].config(text=f"Data de Nascimento:{profile.nascimento}")
+        self.personal_infos[4].config(text=f"Contato:{profile.contato}")
+
+        self.adress_infos[0].config(text=f"CEP:{profile.CEP}")
+        self.adress_infos[1].config(text=f"Rua:{profile.rua}")
+        self.adress_infos[2].config(text=f"Numero:{profile.numero}")
+        self.adress_infos[3].config(text=f"Complemento:{profile.complemento}")
+        self.adress_infos[4].config(text=f"Bairro:{profile.bairro}")
+        self.adress_infos[5].config(text=f"Cidade:{profile.cidade}")
+        self.adress_infos[6].config(text=f"UF:{profile.UF}")
+
+    def build_window(self):
+        self.title.grid(row=0,columnspan=2,pady=10)
+
+        for i in range(len(self.personal_infos)):
+            self.personal_infos[i].grid(row=i+1,column=1,pady=10)
+
+        for i in range(len(self.adress_infos)):
+            self.adress_infos[i].grid(row=i+1,column=2,pady=10)
+
+        self.returnbutton.grid(row = 8, columnspan=2,pady=10)
